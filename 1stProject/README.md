9ee7add5b0bdde1526f5682ac06729e6779e1af8
adityap86 <adityapatel.2006@gmail.com>
2017-04-20 10:31:56 +0530

    Added read me and edited miniflow with backprop

---
+++ 1stProject/README.md
@@ -0,0 +1,4 @@
+Part of Udacity Deep Learning course. Simple implementation of neural network with one hidden layer.
+Dataset: https://archive.ics.uci.edu/ml/datasets/Bike+Sharing+Dataset
+Number of hidden layer: 1
+Number of hidden nodes: 10
--- miniflow/nn.py
+++ miniflow/nn.py
@@ -1,103 +1,44 @@
 """
-This script builds and runs a graph with miniflow.
+Test your network here!
 
-Implemented (x+y) + y
+No need to change this code, but feel free to tweak it
+to test your network!
 
-"""
-
-from miniflow import *
-
-# Example 1
-
-x, y = Input(), Input()
-
-f = Add(x, y)
-f1 = Add(y,f)
-
-feed_dict = {x: 10, y: 5}
-
-sorted_nodes = topological_sort(feed_dict)
-output = forward_pass(f1, sorted_nodes)
-
-# NOTE: because topological_sort set the values for the `Input` nodes we could also access
-# the value for x with x.value (same goes for y).
-print("{} + {} -> {} (according to miniflow)".format(feed_dict[x], feed_dict[y], output))
-
-# Example 2
-
-x, y, z = Input(), Input(), Input()
-
-f = Add(x, y, z)
-
-feed_dict = {x: 4, y: 5, z: 10}
-
-graph = topological_sort(feed_dict)
-output = forward_pass(f, graph)
-
-# should output 19
-print("{} + {} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], feed_dict[z], output))
-
-
-# Example 3 (With Numpy Linear)
-"""
-The setup is similar to the prevous `Linear` node you wrote
-except you're now using NumPy arrays instead of python lists.
-
-Update the Linear class in miniflow.py to work with
-numpy vectors (arrays) and matrices.
-
-
+Make your changes to backward method of the Sigmoid class in miniflow.py
 """
 
 import numpy as np
 from miniflow import *
 
 X, W, b = Input(), Input(), Input()
-
+y = Input()
 f = Linear(X, W, b)
-
-X_ = np.array([[-1., -2.], [-1, -2]])
-W_ = np.array([[2., -3], [2., -3]])
-b_ = np.array([-3., -5])
-
-feed_dict = {X: X_, W: W_, b: b_}
-
-graph = topological_sort(feed_dict)
-output = forward_pass(f, graph)
-
-"""
-Output should be:
-[[-9., 4.],
-[-9., 4.]]
-"""
-print(output)
-
-
-# Example 4 Cost functions calculations
-
-"""
-Test your MSE method with this script!
-
-No changes necessary, but feel free to play
-with this script to test your network.
-"""
-
-from miniflow import *
-
-y, a = Input(), Input()
+a = Sigmoid(f)
 cost = MSE(y, a)
 
-y_ = np.array([1, 2, 3])
-a_ = np.array([4.5, 5, 10])
+X_ = np.array([[-1., -2.], [-1, -2]])
+W_ = np.array([[2.], [3.]])
+b_ = np.array([-3.])
+y_ = np.array([1, 2])
 
-feed_dict = {y: y_, a: a_}
+feed_dict = {
+    X: X_,
+    y: y_,
+    W: W_,
+    b: b_,
+}
+
 graph = topological_sort(feed_dict)
-# forward pass
-forward_pass(graph)
+forward_and_backward(graph)
+# return the gradients for each Input
+gradients = [t.gradients[t] for t in [X, y, W, b]]
 
 """
 Expected output
 
-23.4166666667
+[array([[ -3.34017280e-05,  -5.01025919e-05],
+       [ -6.68040138e-05,  -1.00206021e-04]]), array([[ 0.9999833],
+       [ 1.9999833]]), array([[  5.01028709e-05],
+       [  1.00205742e-04]]), array([ -5.01028709e-05])]
 """
-print(cost.value)
+print(gradients)
